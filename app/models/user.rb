bdfb9eb245a48f436fdcee9929bac8d6498d6003
Melissa Wilcox <melissa@emdubb.co>
2016-01-30 23:14:13 -0800

    add case insensitive validation and require a name

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ActiveRecord::Base
   has_many :lines, dependent: :destroy
   has_secure_password
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
+
+  scope :ci_find, lambda { |attribute, value| where("lower(#{attribute}) = ?", value.downcase).first }
+
 end
