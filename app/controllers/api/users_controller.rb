60757ceaf7f4fbaacc82b3ec04f5cd840b768d84
Melissa Wilcox <melissa@emdubb.co>
2016-01-30 17:11:01 -0800

    add methods for new user, token, and current user

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -2,4 +2,40 @@ class Api::UsersController < ApplicationController
 
   before_action :authorize, except: [:create, :token]
 
+  # POST /api/users
+  def create
+    user = User.create(user_params)
+
+    if user.save
+      token
+    else
+      render status: :unprocessable_entity
+    end
+  end
+
+  # POST /api/token
+  def token
+    user = User.find_by(email: user_params[:email])
+
+    if user && user.authenticate(user_params[:password])
+      payload = {
+        email: user.email
+      }
+      render json: {token: AuthToken.encode(payload)}
+    else
+      render status: :unauthorized
+    end
+  end
+
+  # GET /api/me
+  def me
+    @user = User.find_by(email: @credentials[:email])
+    render status: :not_found unless @user
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password)
+    end
+
 end
