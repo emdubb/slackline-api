f78df885c53be690071b51a89c4a58b465b6ab61
Melissa Wilcox <melissa@emdubb.co>
2016-01-31 01:34:31 -0800

    fix password regex

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ActiveRecord::Base
   validates :name, presence: true, uniqueness: { case_sensitive: false }
 
   validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, on: :create
-  validates_format_of :password, with: /\A[a-zA-Z0-9\!\*\?\$]\A{8,20}/, on: :create
+  validates_format_of :password, with: /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s).{4,12}/, on: :create
   validates_format_of :name, with: /\A[a-zA-Z0-9]\S{3,12}/, on: :create
 
   scope :ci_find, lambda { |attribute, value| where("lower(#{attribute}) = ?", value.downcase).first }
