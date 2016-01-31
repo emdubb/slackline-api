5318534f51a290c79115a42d269d1673188c96d5
Melissa Wilcox <melissa@emdubb.co>
2016-01-31 01:35:50 -0800

    unnest posts and comments routes remove lines index

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,11 @@ Rails.application.routes.draw do
 
   namespace :api do
     resources :users, only: [:index, :create, :show, :update, :destroy], shallow: true do
-      resources :lines, only: [:index, :create, :update, :destroy], shallow: true do
-        resources :posts, only: [:index, :create, :show, :update], shallow: true do
-          resources :comments, only: [:index, :create, :update, :destroy]
-        end
-      end
+      resources :lines, only: [:create, :update, :destroy]
+    end
+
+    resources :posts, only: [:index, :create, :show, :update], shallow: true do
+      resources :comments, only: [:index, :create, :update, :destroy]
     end
 
     get 'me', to: 'users#me'
