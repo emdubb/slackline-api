d7eb3aace61cf41e410acfd791975b0d7780b615
Melissa Wilcox <melissa@emdubb.co>
2016-01-31 02:02:59 -0800

    add index method and line params

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -2,4 +2,15 @@ class Api::PostsController < ApplicationController
 
   before_action :authorize
 
+  def index
+    posts = Post.all
+    render json: posts
+  end
+
+  private
+    def post_params
+      params.require(:post).permit(:latitude, :longitude, :location, :difficulty,
+        :message, :started_at, :finished_at, :line_id)
+    end
+
 end
