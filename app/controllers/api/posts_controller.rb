dbe944384079d55d1990eef5ccd1255c4debd5b9
Melissa Wilcox <melissa@emdubb.co>
2016-01-31 02:58:38 -0800

    add method to create post

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -2,9 +2,31 @@ class Api::PostsController < ApplicationController
 
   before_action :authorize
 
+  # GET /api/posts
   def index
     posts = Post.all
     render json: posts
+  end
+
+  def create
+    if params[:line_id]
+      post = Post.create(
+        location: params[:post]["location"],
+        difficulty: params[:post]["difficulty"],
+        message: params[:post]["message"],
+        started_at: Time.now,
+        line_id: [:line_id]
+        )
+      if post.valid? && post.save
+        render json: post
+      elsif !post.valid?
+        render json: line.errors.messages
+      else
+        render status: :unprocessable_entity
+      end
+    else
+      render json: {message: "Missing id for line to post"}
+    end
   end
 
   private
