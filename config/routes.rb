0634a319c35b482e2320efffcd3023c1fbe86a6e
Melissa Wilcox <melissa@emdubb.co>
2016-01-30 16:31:48 -0800

    add show route to posts

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   namespace :api do
     resources :users, only: [:index, :create, :show, :update, :destroy], shallow: true do
       resources :lines, only: [:index, :create, :update, :destroy], shallow: true do
-        resources :posts, only: [:index, :create, :update], shallow: true do
+        resources :posts, only: [:index, :create, :show, :update], shallow: true do
           resources :comments, only: [:index, :create, :update, :destroy]
         end
       end
