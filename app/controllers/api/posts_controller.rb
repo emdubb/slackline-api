700957952c679729327fa4875a385bd53b96d4d6
Melissa Wilcox <melissa@emdubb.co>
2016-01-31 03:16:58 -0800

    prevent new post if a line is currently posted

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -10,19 +10,24 @@ class Api::PostsController < ApplicationController
 
   def create
     if params[:line_id]
-      post = Post.create(
-        location: params[:post]["location"],
-        difficulty: params[:post]["difficulty"],
-        message: params[:post]["message"],
-        started_at: Time.now,
-        line_id: [:line_id]
-        )
-      if post.valid? && post.save
-        render json: post
-      elsif !post.valid?
-        render json: line.errors.messages
+      line = Line.find(params[:line_id])
+      if line.posts.any? { |posts| !posts.finished_at }
+        render json: { message: "That line is already posted!"}
       else
-        render status: :unprocessable_entity
+        post = Post.create(
+          location: params[:post]["location"],
+          difficulty: params[:post]["difficulty"],
+          message: params[:post]["message"],
+          started_at: Time.now,
+          line_id: params[:line_id]
+          )
+        if post.valid? && post.save
+          render json: post
+        elsif !post.valid?
+          render json: line.errors.messages
+        else
+          render status: :unprocessable_entity
+        end
       end
     else
       render json: {message: "Missing id for line to post"}
