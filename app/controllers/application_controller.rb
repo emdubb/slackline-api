7508cbb023fa8cb81335a1b98672b787a96f505f
Melissa Wilcox <melissa@emdubb.co>
2016-01-30 17:09:54 -0800

    add authorization method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,18 @@
+require 'auth_token'
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session
+
+  def authorize
+    auth_header = request.headers["Authorization"]
+
+    if auth_header
+      auth_token = auth_header.split(' ').last
+      @credentials = AuthToken.decode(auth_token)
+    else
+      render status: :unauthorized
+    end
+  end
+
 end
